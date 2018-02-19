68fa31a3479af0f17f270954c83b5f076c1cb6c0
John <j24oh.uw@gmail.com>
2018-02-19 09:31:46 -0500

    first commit

---
+++ filtering.py
@@ -0,0 +1,100 @@
+import pymongo
+import pandas as pd
+from pymongo import MongoClient
+
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from gensim import corpora
+
+import pickle
+
+client = MongoClient()
+db = client.redditCrawler
+collection = db.data_test1
+
+def remove_posts(data, index_list):
+    data = data.drop(index_list)
+    return data.reset_index(drop=True)
+
+data = pd.DataFrame(list(collection.find()))
+mod_posts = [i for i in range(len(data)) if 'moronic Monday' in data['title'][i]]
+
+#remove all the mod posts that include 'moronic Monday'
+data = remove_posts(data, mod_posts)
+titles = data['title']
+content = data['post']
+comments = data['comments']
+
+# collect only the comments without vote scores, dates, etc
+comments_in_thread = []
+for index, thread in enumerate(comments):
+    aggregate = []
+    for comment in thread:
+        if type(comment['comment_reply']) == str:
+            aggregate.append(comment['comment_reply'].lower())
+    comments_in_thread.append(aggregate)
+
+comments = comments_in_thread
+#number of titles and post need to be the same
+assert len(titles) == len(content) 
+assert len(comments) == len(content)
+
+
+#preprocess
+stop_words = stopwords.words('english')
+stop_words.append('would')
+#stop_words.extend(['would', 'people', 'money'])
+
+#Function to clean off each dataset item; stop words (what, if, is, where, how, I, she)
+def preprocess(text):
+    #no content/nan/len of 0
+    text = text.lower()
+    text = word_tokenize(text)
+    text = [word for word in text if word not in stop_words] 
+    text = [word for word in text if word.isalpha()]
+    return text
+
+#pass titles and comments through pre-processor
+titles = [preprocess(title) for title in titles]
+posts = [preprocess(text) for text in content]
+
+# process comments
+comments = [[preprocess(comment) for comment in thread] for thread in comments]
+
+# form a list of dictionaries for each title, compile
+# each word and its corresponding frequencies in the post's comment section
+list_of_dict = []
+for index, title in enumerate(titles):
+    text = ''
+    bag_of_words = set(title)
+    text = ' '.join(comments_in_thread[index])
+    dictionary = {word:text.count(word) for word in bag_of_words if text.count(word) > 0}
+    list_of_dict.append(dictionary)
+
+title_keyword = [max(Dict, key=Dict.get) if len(Dict) > 0 else 0 for Dict in list_of_dict]
+##keywords = [word for word in keywords if word != 0]
+
+#title 16 has fb and facebook
+
+#count the number of keywords in the comment section
+def count_keywords(comments, keywords):
+    sample = ' '.join(comments).split()
+    return {word: sample.count(word) for word in keywords if sample.count(word) > 0}
+
+keyword_dict = [count_keywords(comments, title_keyword) for comments in comments_in_thread]
+
+for index, thread in enumerate(keyword_dict):
+    #normalize each keyword by the number of words present
+    df = pd.DataFrame()
+    df['word'] = thread.keys()
+    df['count'] = thread.values()
+    df = df.sort_values('count', ascending = False)[:20]
+    df['count'] = df['count']/len(comments[index])
+    keyword_dict[index] = df.reset_index(drop=True)
+
+#save varialbes
+variables = [data['title'], titles, posts, comments, comments_in_thread,
+             list_of_dict, title_keyword, keyword_dict]
+
+with open('variables.txt', 'wb') as fp:
+    pickle.dump(variables, fp)
